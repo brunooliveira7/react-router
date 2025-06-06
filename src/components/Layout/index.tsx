253143820f6b8422723161020931234d43e7727f
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-06-06 14:34:37 -0300

    feat: Implement layout component with header and footer
    This commit introduces a layout component to provide a consistent structure across different pages.
    
    - Creates a `Layout` component in `src/components/Layout/index.tsx` with a header displaying a user greeting and a footer with a copyright notice.
    - Adds corresponding styles in `src/components/Layout/styles.css` for the header and footer.
    - Modifies `src/routes/AppRoutes.tsx` to wrap the existing routes (Home, Products, Details) within the new `Layout` component, ensuring a consistent layout for these pages.

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+import { Outlet } from "react-router";
+
+export function Layout() {
+  return (
+    <div>
+      <header className="user">
+        <p>Olá, Bruno</p>
+      </header>
+
+      <Outlet />
+
+      <footer>
+        <span>Todos os direitos reservados</span>
+      </footer>
+    </div>
+  );
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,13 +3,16 @@ import { Home } from "../pages/Home";
 import { Products } from "../pages/Products";
 import { NotFound } from "../pages/NotFound";
 import { Details } from "../pages/Details";
+import { Layout } from "../components/Layout";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" index element={<Home />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/details/:id" element={<Details />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" index element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Route>
 
       <Route path="*" element={<NotFound />} />
     </Routes>
