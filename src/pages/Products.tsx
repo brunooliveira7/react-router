47dfea28ea478fba4773d36b3d10cf66e1bc14fb
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-06-06 09:33:53 -0300

    feat: Implement category filtering on products page
    This commit introduces category filtering on the products page.
    
    - Adds a category filter link to the home page, linking to the products page with a "category=tvs" query parameter.
    - Implements logic in the Products component to read the "category" query parameter from the URL using `useSearchParams`.
    - Displays the selected category on the products page if a category parameter is present in the URL.
    - Adds styling for the category display, including color and text transformation.

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,11 +1,22 @@
+import { useSearchParams } from "react-router";
+
 export function Products() {
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category");
+
   return (
     <div>
       <nav>
         <a href="/">Voltar</a>
       </nav>
 
-      <h1>Página Produtos</h1>
+      <h1>Produtos</h1>
+
+      {category && ( // se a categoria existir, renderiza o span
+        <span>
+          Categoria<strong> {category}</strong>
+        </span>
+      )}
     </div>
   );
 }
